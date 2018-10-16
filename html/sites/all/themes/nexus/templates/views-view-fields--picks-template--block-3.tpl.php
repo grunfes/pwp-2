322d72d76f82affc5d0d859e46d130cc27a5213a
grunfes <grunfes@yahoo.com>
2018-10-16 19:13:03 +0200

    initial commit

---
+++ html/sites/all/themes/nexus/templates/views-view-fields--picks-template--block-3.tpl.php
@@ -0,0 +1,10 @@
+<h3 class="left w100"><?php print $fields['title']->content;?></h3>
+
+<div class="match clearfix w100 m5-0">
+    <?php
+
+    ?>
+</div>
+
+<div class="left clearfix w100"><label></label><?php print $fields['views_conditional']->content;?></div>
+<div class="left clearfix w100"><a href="/editors-picks">Editor's Picks</a></div>
